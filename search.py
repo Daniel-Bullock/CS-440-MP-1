b6e4dfe518d504136eb7245a778e3fdfa581f3b0
Daniel <dbullock377@comcast.net>
2020-09-04 22:29:48 -0500

    test search

--- search.py
+++ search.py
@@ -18,6 +18,8 @@ files and classes when code is run, so be careful to not modify anything else.
 # maze is a Maze object based on the maze from the file specified by input filename
 # searchMethod is the search method specified by --method flag (bfs,dfs,astar,astar_multi,fast)
 
+import queue
+
 def search(maze, searchMethod):
     return {
         "bfs": bfs,
@@ -36,7 +38,47 @@ def bfs(maze):
     @return path: a list of tuples containing the coordinates of each state in the computed path
     """
     # TODO: Write your code here
-    return []
+
+
+    start = maze.getStart()
+    q = queue.Queue()
+    q.put(start)
+
+    visited = {}
+
+    predecessors = {}
+    predecessors[start] = None
+
+    while (not q.empty()):
+
+
+
+        first = q.get()
+        current_pred = first
+        visited[first] = 1
+        neighbors = maze.getNeighbors(first[0], first[1])
+
+        for neighbor in neighbors:
+            if (not neighbor in visited) and (maze.isValidMove(neighbor[0], neighbor[1])):
+                predecessors[neighbor] = current_pred
+                visited[neighbor] = 1
+                q.put(neighbor)
+
+    dots = maze.getObjectives()
+
+    goal = maze.getObjectives()[0]
+    path = []
+    path.append(goal)
+    current = goal
+
+    while (current != start):
+        path.append(predecessors[current])
+        current = predecessors[current]
+
+    path.append(start)
+
+    return path
+
 
 
 def astar(maze):
